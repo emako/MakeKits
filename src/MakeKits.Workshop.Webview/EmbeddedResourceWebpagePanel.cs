c2765902cac68708b8ca57afa8ee0fd6b69511a2
ema <mccoy39082@163.com>
2026-06-24 10:32:36 +0800

    Serve embedded home page, add route & fixes

--- src/MakeKits.Workshop.Webview/EmbeddedResourceWebpagePanel.cs
+++ src/MakeKits.Workshop.Webview/EmbeddedResourceWebpagePanel.cs
@@ -20,12 +20,20 @@ public abstract class EmbeddedResourceWebpagePanel : WebpagePanel
     protected EmbeddedResourceWebpagePanel()
     {
         _resources = EmbeddedResourceLoader.LoadResources(GetType().Assembly, ResourcePrefix);
-        _homePage = EmbeddedResourceLoader.ReadResource(_resources, HomePageResourcePath) ?? [];
+        _homePage = _resources[HomePageResourcePath];
     }
 
     public virtual void NavigateToHomePage()
     {
         NavigateToUri(new Uri(VirtualHost));
+    }
+
+    protected override void Webview_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e)
+    {
+        if (new Uri(e.Uri).Authority.Equals(new Uri(VirtualHost).Host, StringComparison.OrdinalIgnoreCase))
+            return;
+
+        base.Webview_NavigationStarting(sender, e);
     }
 
     protected override void WebView_WebResourceRequested(object? sender, CoreWebView2WebResourceRequestedEventArgs args)
@@ -39,23 +47,15 @@ public abstract class EmbeddedResourceWebpagePanel : WebpagePanel
                 return;
 
             string absolutePath = Uri.UnescapeDataString(requestedUri.AbsolutePath);
-            if (absolutePath == "/")
+
+            if (absolutePath == "/" && requestedUri.Authority.Equals(new Uri(VirtualHost).Host, StringComparison.OrdinalIgnoreCase))
             {
-                if (requestedUri.Authority.Equals("makekits.local", StringComparison.OrdinalIgnoreCase))
-                {
-                    args.Response = _webView.CoreWebView2.Environment.CreateWebResourceResponse(
-                        new MemoryStream(_homePage), 200, "OK", MimeTypes.GetContentType(".html"));
-                }
-                else
-                {
-                    base.WebView_WebResourceRequested(sender, args);
-                }
+                args.Response = _webView.CoreWebView2.Environment.CreateWebResourceResponse(
+                    new MemoryStream(_homePage), 200, "OK", MimeTypes.GetContentType(".html"));
             }
-            else
-            {
-                if (TryCreateEmbeddedResourceResponse(absolutePath, out CoreWebView2WebResourceResponse? response))
-                    args.Response = response;
-            }
+
+            if (TryCreateEmbeddedResourceResponse(absolutePath, out CoreWebView2WebResourceResponse? response))
+                args.Response = response;
         }
         catch (Exception exception)
         {

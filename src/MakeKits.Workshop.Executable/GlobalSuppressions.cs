f2faf3ff515222e09ffc76b3173c616daf8fba7e
ema <mccoy39082@163.com>
2026-07-02 16:25:59 +0800

    Add child process tracer using job objects

--- src/MakeKits.Workshop.Executable/GlobalSuppressions.cs
+++ src/MakeKits.Workshop.Executable/GlobalSuppressions.cs
@@ -8,3 +8,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE0057:Use range operator")]
 [assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
 [assembly: SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize")]
+[assembly: SuppressMessage("Interoperability", "SYSLIB1054:Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression")]

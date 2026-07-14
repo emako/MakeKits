0a6467b764defc2be0c2646f3e78f6699789377b
ema <mccoy39082@163.com>
2026-07-14 19:11:28 +0800

    Extract added NuGet package in local cache

---
+++ build/nuget_add.ps1
@@ -0,0 +1,43 @@
+﻿param(
+    [string]$ApiKey
+)
+
+Set-Location $PSScriptRoot
+
+Write-Host @"
+███╗   ██╗██╗   ██╗ ██████╗ ███████╗████████╗
+████╗  ██║██║   ██║██╔════╝ ██╔════╝╚══██╔══╝
+██╔██╗ ██║██║   ██║██║  ███╗█████╗     ██║   
+██║╚██╗██║██║   ██║██║   ██║██╔══╝     ██║   
+██║ ╚████║╚██████╔╝╚██████╔╝███████╗   ██║   
+╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   
+"@
+
+$nupkg = Get-ChildItem -Path . -Name "MakeKits.Tools.*.nupkg" |
+    ForEach-Object {
+        if ($_ -match 'MakeKits\.Tools\.(\d+\.\d+\.\d+)\.nupkg') {
+            [PSCustomObject]@{ File = $_; Version = [version]$matches[1] }
+        }
+    } |
+    Sort-Object Version -Descending |
+    Select-Object -First 1 -ExpandProperty File
+
+if (-not $nupkg) {
+    Write-Host "MakeKits.Tools.*.nupkg not found"
+    exit 1
+}
+
+$version = if ($nupkg -match '(\d+\.\d+\.\d+)\.nupkg') { $matches[1] }
+
+$source = [System.IO.Path]::GetFullPath("$env:USERPROFILE\.nuget\packages\")
+
+Write-Host "Adding MakeKits.Tools v$version ($nupkg) to $source"
+& .\bin\nuget.exe add $nupkg -Source $source
+
+$pkgDir = "$source\makekits.tools\$version"
+$nupkgPath = "$pkgDir\$nupkg"
+Write-Host "Extracting $nupkgPath to $pkgDir"
+& .\bin\7z.exe x $nupkgPath -o"$pkgDir" -y
+
+Write-Host "`nPress any key to exit..."
+[void][System.Console]::ReadKey($true)
